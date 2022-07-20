82260f06d209ceab960fd0f04da2b268af892e78
JimmyLai <jimmy050311@gmail.com>
2022-07-20 22:50:47 +0800

    Initial Commit

---
+++ Parking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parking
+//
+//  Created by Lai Chun Ming on 2022/7/20.
+//  Copyright © 2022 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
